1aecf42c9ca9e61c24b7c2dc3330e7b137b75ba7
Barirah Sundus <barirahsundus76@gmail.com>
2026-09-20 19:01:35 +0530

    Add HighestDegreeStrategy class definition

--- Module_3_InterventionStrategy/include/HighestDegreeStrategy.h
+++ Module_3_InterventionStrategy/include/HighestDegreeStrategy.h
@@ -1,1 +1,20 @@
+#ifndef HIGHESTDEGREESTRATEGY_H
+#define HIGHESTDEGREESTRATEGY_H
 
+#include "InterventionStrategy.h"
+
+using namespace std;
+
+class HighestDegreeStrategy : public InterventionStrategy
+{
+public:
+    int selectUser(
+        const vector<vector<int>>& graph,
+        const vector<int>& status,
+        int remainingBudget
+    ) override;
+
+    string getName() const override;
+};
+
+#endif
