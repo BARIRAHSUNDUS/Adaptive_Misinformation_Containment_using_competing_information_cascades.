ff4b74632897782ee11ab221274d55def0cd2f02
shagungagneja0-cmyk <shagungagneja0@gmail.com>
2026-09-20 18:43:35 +0530

    Update moderatorbudget.h

--- Module_4_SimulationEngine/include/moderatorbudget.h
+++ Module_4_SimulationEngine/include/moderatorbudget.h
@@ -1,1 +1,15 @@
+class ModeratorBudget
+{
+private:
+    int totalBudget;
+    int remainingBudget;
 
+public:
+    ModeratorBudget(int budget);
+
+    bool canIntervene();
+    bool useIntervention();
+
+    int getTotalBudget();
+    int getRemainingBudget();
+};
