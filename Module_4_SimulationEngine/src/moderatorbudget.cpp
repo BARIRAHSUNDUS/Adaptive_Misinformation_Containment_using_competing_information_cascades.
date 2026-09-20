299fc5062c27d176b41f214884e62c787966004a
shagungagneja0-cmyk <shagungagneja0@gmail.com>
2026-09-20 18:45:05 +0530

    Update moderatorbudget.cpp

--- Module_4_SimulationEngine/src/moderatorbudget.cpp
+++ Module_4_SimulationEngine/src/moderatorbudget.cpp
@@ -1,1 +1,39 @@
+#include "ModeratorBudget.h"
 
+ModeratorBudget::ModeratorBudget(int budget)
+{
+    if (budget < 0)
+    {
+        budget = 0;
+    }
+
+    totalBudget = budget;
+    remainingBudget = budget;
+}
+
+bool ModeratorBudget::canIntervene()
+{
+    return remainingBudget > 0;
+}
+
+bool ModeratorBudget::useIntervention()
+{
+    if (remainingBudget <= 0)
+    {
+        return false;
+    }
+
+    remainingBudget--;
+
+    return true;
+}
+
+int ModeratorBudget::getTotalBudget()
+{
+    return totalBudget;
+}
+
+int ModeratorBudget::getRemainingBudget()
+{
+    return remainingBudget;
+}
