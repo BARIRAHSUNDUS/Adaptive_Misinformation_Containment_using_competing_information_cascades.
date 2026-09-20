bf89d9f31204cf2f0df50848a06efd504692e069
avnisingh8694 <avnisingh312006@gmail.com>
2026-09-20 18:21:07 +0530

    Update CascadeModel.h

--- Module_2_CascadeModel/include/CascadeModel.h
+++ Module_2_CascadeModel/include/CascadeModel.h
@@ -1,1 +1,30 @@
+#ifndef CASCADEMODEL_H
+#define CASCADEMODEL_H
 
+#include <vector>
+#include "UserState.h"
+
+using namespace std;
+
+class CascadeModel
+{
+private:
+    vector<vector<int>> graph;
+    vector<int> status;
+
+public:
+    CascadeModel(vector<vector<int>> g, int n);
+
+    int spreadMisinformation(
+        int source,
+        double spreadProbability
+    );
+
+    void factCheck(int user);
+
+    int spreadFactCheck(int source);
+
+    void showStatus();
+};
+
+#endif
