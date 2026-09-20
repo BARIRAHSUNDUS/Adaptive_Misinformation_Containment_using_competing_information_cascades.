cab2b87a7db6f644efd7741b2c3e25b73fa0ae8d
vaniagarwal107-ship-it <vaniagarwal107@gmail.com>
2026-09-20 18:01:42 +0530

    Define SocialGraph class with user management methods

--- Module_1_SocialGraph/include/SocialGraph.h
+++ Module_1_SocialGraph/include/SocialGraph.h
@@ -1,1 +1,27 @@
+#pragma once
 
+#include <vector>
+#include "User.h"
+
+class SocialGraph
+{
+private:
+    std::vector<User> users;
+    std::vector<std::vector<int>> adjacencyList;
+
+public:
+
+    void addUser(int id);
+
+    void addConnection(int u, int v);
+
+    void displayGraph();
+
+    void getNeighbours(int user);
+
+    int getDegree(int user);
+
+    int getMaxDegreeUser();
+
+    void BFS(int startUser);
+};
