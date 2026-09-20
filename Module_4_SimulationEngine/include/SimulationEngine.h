fa6e341a9f44b698060e658d0f315fdfcd1f4b7f
shagungagneja0-cmyk <shagungagneja0@gmail.com>
2026-09-20 18:44:19 +0530

    Update SimulationEngine.h

--- Module_4_SimulationEngine/include/SimulationEngine.h
+++ Module_4_SimulationEngine/include/SimulationEngine.h
@@ -1,1 +1,35 @@
+#include <vector>
+#include "ModeratorBudget.h"
+#include "CascadeModel.h"
 
+using namespace std;
+
+class SimulationEngine
+{
+private:
+    int maxTicks;
+    int warmUpTicks;
+
+    ModeratorBudget budget;
+
+    CascadeModel* cascade;
+
+    vector<int> interventionTicks;
+
+    void createInterventionSchedule();
+
+    bool isInterventionTick(int tick);
+
+public:
+
+    SimulationEngine(
+        int maxTicks,
+        int warmUpTicks,
+        int budget,
+        CascadeModel* cascadeModel
+    );
+
+    void showInterventionSchedule();
+
+    void run();
+};
